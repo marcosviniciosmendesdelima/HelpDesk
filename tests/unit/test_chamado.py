28f419c9d89587ef1a1fbf54bfdc041ae18d8b68
FelipeAlbuquerque900 <felipealbuquerque161@gmail.com>
2026-05-26 18:51:52 -0300

    fix: ajusta testes unitários para novo campo prioridade

--- tests/unit/test_chamado.py
+++ tests/unit/test_chamado.py
@@ -2,18 +2,20 @@ from should_dsl import should
 from src.domain.entities.chamado import Chamado
 
 def test_deve_criar_chamado_com_status_aberto_por_padrao():
-    # Arrange
-    chamado = Chamado(titulo="Erro no sistema", descricao="O sistema travou")
-   
-    # Act & Assert 
-    chamado.status |should| be("Aberto")
+    chamado = Chamado(
+    titulo="Erro no sistema",
+    descricao="O sistema travou",
+    prioridade="Alta"
+)
+    assert chamado.status == "Aberto"
 
 def test_deve_permitir_definir_prioridade_no_chamado():
-    # Arrange
-    chamado = Chamado(titulo="Urgente", descricao="Servidor fora do ar")
+    chamado = Chamado(
+    titulo="Urgente",
+    descricao="Servidor fora do ar",
+    prioridade="Alta"
+    )
    
-    # Act
     chamado.prioridade_valor = "Alta"
    
-    # Assert 
-    chamado.prioridade_valor |should| be("Alta")
+    assert chamado.prioridade_valor == "Alta"
